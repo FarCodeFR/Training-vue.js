61fa5985de804ee92ac3dc52c8259809311a1c80
FarCodeFR <Timo-44@hotmail.com>
2025-06-26 14:04:07 +0200

    start

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import './style.css';
+import MonButton from "./components/MonButton.vue";
+
+
+const count = ref(0);
+function increment() {
+  count.value++
+}
+function decrement() {
+  count.value--
+} 
+function onClick(){
+  alert("Bouton cliqué !")
+}
+</script>
+
+<template>
+  <MonButton label="Clique moi !" @clic="onClick"/>
+  <section class="first-container">
+  <p>{{ count }}</p>
+  <p>{{ count === 10 ? "Vous avez gagné" : "" }}</p>
+  <div class="container-button">
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
+  </div>
+  </section>
+</template>
+
---
+++ src/components/MonButton.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+defineProps<{label: string}>()
+const emit = defineEmits<{(e: 'clic'): void}>()
+</script>
+
+<template>
+  <button @click="emit('clic')">{{ label }}</button>
+</template>
